170adcfccf9bbccfa1282006156d926e8dde8d67
Joao <joaovitor1998alvesmuniz@outlook.com>
2021-05-11 23:14:20 -0300

    ultimo commit eu acho

--- controllers/authAdmin.js
+++ controllers/authAdmin.js
@@ -2,9 +2,8 @@ const User = require("../database/modules/user")
 
 
 const validaAdm = async function (req,res,next){
-
+     try{
      const id = req.header('Authorization');
-
      if(!id){
 
         return res.status(401).send({error: "Porfavador informe o id do usuario"})
@@ -22,7 +21,10 @@ const validaAdm = async function (req,res,next){
        return res.status(401).send({error: "Acesso negado"})
 
       }
+    }catch(err){
 
+      return res.status(400).send({error: "Erro"})
+    }
 }
 
 module.exports = validaAdm
